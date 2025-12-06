c1d85fa560db7fca4b67a05c83c448d02e2bf54e
Piyush <piyushshrivastava22ec27@gmail.com>
2025-12-07 01:11:23 +0530

    dynamic data via backend api to show recent user orders

--- client/src/components/home/OrderList.jsx
+++ client/src/components/home/OrderList.jsx
@@ -1,36 +1,48 @@
 import React from 'react'
 import { FaCheckDouble, FaCircle } from 'react-icons/fa'
+import {getAvatarName} from '../../utils/index.js'
 
-const OrderList = () => {
+const OrderList = ({key, order}) => {
     return (
         <div className='flex items-center gap-5 mb-2'>
             
             <button className='bg-[#f6b100] text-xl p-2 font-bold rounded-lg'>
-                PS
+                {getAvatarName(order.customerDetails.name)}
             </button>
 
             <div className='w-full flex items-center justify-between'>
 
                 <div className='flex flex-col items-start gap-1'>
                     <h1 className='text-[#f5f5f5] text-lg font-semibold tracking-wide'>
-                        Piyush Shrivastava 
+                        {order.customerDetails.name} 
                     </h1>
                     <p className='text-xs text-[#ababab]'>
-                        8 Items 
+                        {order.items.length} Items 
                     </p>
                 </div>
 
                 <div>
-                    <h1 className='text-[#f6b100] text-xs font-semibold border border-[#f6b100] rounded-lg p-1'>Table No: 3</h1>
+                    <h1 className='text-[#f6b100] text-xs font-semibold border border-[#f6b100] rounded-lg p-1'>
+                        Table No: {order?.table?.tableNo}
+                    </h1>
                 </div>
 
                 <div className='flex flex-col items-end gap-1'>
-                    <p className=' text-green-600'>
-                        <FaCheckDouble className='inline mr-2' /> Ready 
-                    </p>
-                    <p className='text-[#ababab] text-xs'>
-                        <FaCircle className='inline mr-2 text-green-600' /> Ready to Serve 
-                    </p>
+                    {
+                        order.orderStatus === "Ready" ? (
+                            <>
+                            <p className=' text-green-600'>
+                                <FaCheckDouble className='inline mr-2' /> {order.orderStatus} 
+                            </p>
+                            </>
+                        ) : (
+                            <>
+                            <p className=' text-yellow-600'>
+                                <FaCircle className='inline mr-2' /> {order.orderStatus} 
+                            </p>
+                            </>
+                        )
+                    }
                 </div>
 
             </div>
--- client/src/components/home/RecentOrder.jsx
+++ client/src/components/home/RecentOrder.jsx
@@ -1,8 +1,25 @@
 import  React from "react";
 import { FaSearch } from "react-icons/fa";
 import OrderList from "./OrderList";
+import { enqueueSnackbar } from "notistack";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { getAllOrder } from "../../https";
 
 const RecentOrders = () => {
+
+    const {data:resData, error} = useQuery({
+        queryKey: ["orders"],
+
+        queryFn: async () => {
+            return await getAllOrder()
+        },
+
+        placeholderData: keepPreviousData
+    })
+    if(error){
+        enqueueSnackbar("Something went wrong", {variant : "error"})
+    }
+
     return (
         <div className="px-5 mt-4">
             <div className="w-full h-[370px] bg-[#1a1a1a] rounded-lg p-4">
@@ -16,7 +33,7 @@ const RecentOrders = () => {
                     </a>
                 </div>
 
-                <div className="flex items-center bg-[#1f1f1f] rounded-md px-3 py-2">
+                {/* <div className="flex items-center bg-[#1f1f1f] rounded-md px-3 py-2">
                     <FaSearch className="text-[#b5b5b5] mr-2" size={14} />
                     <input
                         type="text"
@@ -24,20 +41,21 @@ const RecentOrders = () => {
                         className="w-full bg-transparent text-[#f5f5f5] placeholder-[#8a8a8a] outline-none"
                     />
 
-                </div>
-                    
-                <div className="mt-5 px-3 overflow-y-scroll h-[225px] no-scrollbar">
-                    <OrderList />
-                    <OrderList />
-                    <OrderList />
-                    <OrderList />
-                    <OrderList />
-                    <OrderList />
-                    <OrderList />
-                    <OrderList />
-                    <OrderList />
-                    <OrderList />
-                    <OrderList />
+                </div> */}
+                
+                <div className="mt-5 px-3 overflow-y-scroll h-[275px] no-scrollbar">
+                    {
+                        resData?.data.data.length > 0 ? (
+                            resData.data.data.map((order) => {
+
+                                return <OrderList key={order._id} order={order} />
+                            })
+                        ) : (
+                            <p className="text-gray-400 text-lg tracking-wide font-medium">
+                                No orders present at the moment
+                            </p>
+                        )
+                    }
                 </div>
 
             </div>
