739dc212d88b65590d974ad9263349739e78c839
Piyush <piyushshrivastava22ec27@gmail.com>
2025-12-07 03:58:23 +0530

    added frontend deployed URL to regulate CORS

--- server/app.js
+++ server/app.js
@@ -22,7 +22,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.use(cors({
-    origin : ['http://localhost:5173'],
+    origin : [
+        'http://localhost:5173',
+        'https://pos-master-client.onrender.com/'
+    ],
     credentials : true,
 }))
 
